22e81d6f21663739a4b8ee8893bd8611e73592a3
Bonnie <55579065+bonnie-bg@users.noreply.github.com>
2022-09-02 12:41:18 +0000

    Add files via upload

---
+++ Admin/admin_Scripts/admin.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $(".span1").click(function() {
+        $(".dropdown").slideToggle(500);
+    });
+});
+
+$('.sidenav li').click(function() {
+    $('.sidenav li').removeClass("active");
+    $(this).addClass("active");
+});
+
+
+$(".menu-icon").click(function(e) {
+    e.preventDefault();
+    $(".menu-icon").toggleClass("menuicon");
+    $(".main").toggleClass("main-width");
+    $(".sidebar").toggleClass("active1");
+    $(".sidenav li a").toggleClass("anchor");
+    $(".sidenav li").toggleClass("lislide");
+    $(".sidenav p").toggleClass("apphide");
+    $(".logo span").toggleClass("headspan");
+    $(".logo").toggleClass("lm");
+
+});
+$(document).ready(function() {
+    $(".user").click(function() {
+        $(".profile-div").toggle(1000);
+    });
+    $(".bell").click(function() {
+        $(".notification-div").toggle(1000);
+    });
+});
